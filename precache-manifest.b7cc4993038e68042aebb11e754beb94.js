90b46f6640a98dacd621ef27d400d5fe5f221514
iliuta.stoica@everymatrix.com <iliuta.stoica@everymatrix.com>
2019-03-21 17:02:13 +0200

    Updates

---
+++ precache-manifest.b7cc4993038e68042aebb11e754beb94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a70ab81c67defc9fa7dc",
+    "url": "/deploy-react/static/js/runtime~main.5de6419e.js"
+  },
+  {
+    "revision": "016ce5d6f96806499adc",
+    "url": "/deploy-react/static/js/main.77fc9204.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/deploy-react/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "016ce5d6f96806499adc",
+    "url": "/deploy-react/static/css/main.cfc2674a.chunk.css"
+  },
+  {
+    "revision": "2bea7e007e170f3872281c3f1764cc18",
+    "url": "/deploy-react/index.html"
+  }
+];
